f4532d8b359b5089729cbe70cf8a705007abf33d
Cydnee Owens <cks410@yahoo.com>
2020-10-07 13:24:27 -0600

    Add dish names method and test

--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -59,4 +59,13 @@ class RestaurantTest < Minitest::Test
     assert_equal true, restaurant1.open_for_lunch?
     assert_equal false,restaurant2.open_for_lunch?
   end 
+
+  def test_it_can_show_dish_names 
+    restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+    restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+    restaurant2.add_dish('Burrata')
+    restaurant2.add_dish('Pizzetta')
+    restaurant2.add_dish('Ravioli')
+    assert_equal ["BURRATA", "PIZZETTA", "RAVIOLI"], restaurant2.menu_dish_names
+  end
 end
